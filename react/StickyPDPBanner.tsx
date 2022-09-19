37ef6b550ef20e6e49675d7a7aee5fdfcf3efd00
Web Developer <useraccount@WebDeveloperMBP.local>
2022-09-19 12:57:51 -0500

    Refactoring

--- react/StickyPDPBanner.tsx
+++ react/StickyPDPBanner.tsx
@@ -22,32 +22,27 @@ const StickyPDPBanner: StorefrontFunctionComponent<StickyPDPBannerProps> = ({ })
   const containerId = "sticky-pdp-banner-container";
 
   useEffect(() => {
-    if (canUseDOM) {
-      const productName: Element = document.getElementsByClassName("vtex-store-components-3-x-productBrand")[0];
-      const productNameString: string = productName.innerHTML.replace("<!-- -->", "");
-      setProductName(productNameString);
-    }
+    if (!canUseDOM) return;
+
+    const productName: Element = document.getElementsByClassName("vtex-store-components-3-x-productBrand")[0];
+    const productNameString: string = productName.innerHTML.replace("<!-- -->", "");
+    setProductName(productNameString);
   })
 
   // Create / Destroy Intersection Observer
   useEffect(() => {
-    let div: HTMLElement;
-    let observer: IntersectionObserver;
+    if (!canUseDOM) return;
 
-    if (canUseDOM) {
-      const halfwayPoint = window.innerHeight / 2;
-      div = document.getElementById(containerId)!;
-      if (div === null) return;
+    const halfwayPoint = window.innerHeight / 2;
+    const div: HTMLElement = document.getElementById(containerId)!;
+    if (!div) return;
 
-      observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          const intersecting = entry.isIntersecting;
-          setIsStuck(intersecting ? false : true);
-        });
-      }, { threshold: 1, rootMargin: `0px 0px ${halfwayPoint}px 0px` });
+    const observer: IntersectionObserver = new IntersectionObserver(entries => {
+      const entry: IntersectionObserverEntry = entries[0];
+      setIsStuck(!entry.isIntersecting);
+    }, { threshold: 1, rootMargin: `0px 0px ${halfwayPoint}px 0px` });
 
-      observer.observe(div!);
-    }
+    observer.observe(div);
 
     return () => {
       observer.unobserve(div);
@@ -56,58 +51,50 @@ const StickyPDPBanner: StorefrontFunctionComponent<StickyPDPBannerProps> = ({ })
 
   // Change classes when Stuck and Unstuck
   useEffect(() => {
-    if (canUseDOM) {
-      const div: HTMLElement = document.getElementById(containerId)!;
-      isStuck ? div.classList.add(classPrefix + "isStuck") : div.classList.remove(classPrefix + "isStuck");
-    }
+    if (!canUseDOM) return;
+    const div: HTMLElement = document.getElementById(containerId)!;
+    isStuck ? div.classList.add(classPrefix + "isStuck") : div.classList.remove(classPrefix + "isStuck");
+
   }, [isStuck]);
 
   const buildButtons = () => {
-    setHasDescription(false);
-    setHasFeatures(false);
-    setHasSpecs(false);
-    setHasExtras(false);
-    setHasArticles(false);
-    setHasReviews(false);
+    if (!canUseDOM) return;
 
-    if (canUseDOM) {
-      const description: Boolean = !!document.getElementById("productOverview");
-      const features: Boolean = !!document.getElementsByClassName("eriksbikeshop-product-attribute-0-x-productAttributeWrapper--pdp-features")[0];
-      const specs: Boolean = !!document.getElementById("specs-section");
-      const extras: Boolean = !!document.getElementById("eriks-extras");
-      const articlesElement: Element = document.getElementsByClassName("vtex-wordpress-integration-2-x-relatedPostsBlockContainer")[0];
-      const articles: Boolean = articlesElement ? !!articlesElement.innerHTML : false;
-      const reviews: Boolean = !!document.getElementById("all-reviews");
+    const description: Boolean = !!document.getElementById("productOverview");
+    const features: Boolean = !!document.getElementsByClassName("eriksbikeshop-product-attribute-0-x-productAttributeWrapper--pdp-features")[0];
+    const specs: Boolean = !!document.getElementById("specs-section");
+    const extras: Boolean = !!document.getElementById("eriks-extras");
+    const articlesElement: Element = document.getElementsByClassName("vtex-wordpress-integration-2-x-relatedPostsBlockContainer")[0];
+    const articles: Boolean = articlesElement ? !!articlesElement.innerHTML : false;
+    const reviews: Boolean = !!document.getElementById("all-reviews");
 
-      if (description) setHasDescription(true);
-      if (features) setHasFeatures(true);
-      if (specs) setHasSpecs(true);
-      if (extras) setHasExtras(true);
-      if (articles) setHasArticles(true);
-      if (reviews) setHasReviews(true);
-    }
+    setHasDescription(description);
+    setHasFeatures(features);
+    setHasSpecs(specs);
+    setHasExtras(extras);
+    setHasArticles(articles);
+    setHasReviews(reviews);
   }
 
+  // Debounce for calling buildButtons
   useEffect(() => {
-    let handleScroll: any;
-    if (canUseDOM) {
-      handleScroll = () => {
-        setTimeout(() => {
-          buildButtons();
-        }, 1000)
-      };
-      window.addEventListener('scroll', handleScroll);
-    }
+    if (!canUseDOM) return;
+
+    const handleScroll = () => {
+      window.removeEventListener("scroll", handleScroll);
+      setTimeout(() => {
+        buildButtons();
+      }, 1000)
+    };
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      if (canUseDOM) window.removeEventListener('scroll', handleScroll);
+      if (canUseDOM) window.removeEventListener("scroll", handleScroll);
     }
   });
 
   const handleScrollTop = () => {
-    if (canUseDOM) {
-      window.scrollTo(0, 0);
-    }
+    if (canUseDOM) window.scrollTo(0, 0);
   }
 
   return (
